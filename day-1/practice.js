f3f51d95ce94bd1b6bec4921f46f92c965bf24d4
Eshrak Rahman <eshrakurrahman@gmail.com>
2025-07-29 23:43:30 +0600

    day-one complete

--- day-1/practice.js
+++ day-1/practice.js
@@ -1,40 +1,49 @@
-var createCounter = function(init) {
-    let res = init;
-    let current = init;
-    return {
-        increment: () => {
-            ++current;
-            // current = init;
-            console.log(current);
-            return current;
+// var createCounter = function(init) {
+//     let res = init;
+//     let current = init;
+//     return {
+//         increment: () => {
+//             ++current;
+//             // current = init;
+//             console.log(current);
+//             return current;
             
-        },
-        reset: () => {
-            current = res;
-            console.log(current);
+//         },
+//         reset: () => {
+//             current = res;
+//             console.log(current);
             
-            return res;
-        },
-        decrement: () => {
-            init = current;
-            --current;
-            console.log(current);
+//             return res;
+//         },
+//         decrement: () => {
+//             init = current;
+//             --current;
+//             console.log(current);
             
-            return current;
+//             return current;
             
-        }
-    }
+//         }
+//     }
     
-};
+// };
 
-let count = createCounter(107);
-console.log(count.increment());
-console.log(count.increment());
-console.log(count.decrement());
-console.log(count.decrement());
-console.log(count.increment());
-console.log(count.reset());
-console.log(count.increment());
-console.log(count.increment());
-console.log(count.decrement());
-console.log(count.decrement());
+// let count = createCounter(107);
+// console.log(count.increment());
+// console.log(count.increment());
+// console.log(count.decrement());
+// console.log(count.decrement());
+// console.log(count.increment());
+// console.log(count.reset());
+// console.log(count.increment());
+// console.log(count.increment());
+// console.log(count.decrement());
+// console.log(count.decrement());
+
+// leetcode day 3 problem 
+
+console.log(
+    "I am js file"
+);
+
+
+document.getElementById("someId").innerHTML = "I am something";
