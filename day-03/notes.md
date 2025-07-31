84819e95429b2e8a2f8a42fb266e8a120b506495
eshrak <eshrak@Mds-MacBook-Air-2.local>
2025-07-31 19:24:51 +0600

    leetcode problem solved

---
+++ day-03/notes.md
@@ -0,0 +1,6 @@
+# operator 
+- Symbols + - * /
+# Operands 
+- x + y, x and y are the operands.
+# Expression
+- x = 2 
---
+++ day-03/script.js
@@ -0,0 +1,21 @@
+console.log("Day 03");
+// Arithmetic operator 
+
+let x = 30;
+let y = 3;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x ** y);
+
+let count = 5;
+console.log(count++);
+console.log(count);
+console.log(++count);
+
+let a = 10;
+a += 20;
+
+console.log("cow" && "horse");
