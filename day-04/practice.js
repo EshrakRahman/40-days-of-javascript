e96da97cddf6ed47dc3d2e997cdb0ebfa59022c6
Eshrak Rahman <eshrakurrahman@gmail.com>
2025-08-01 20:06:22 +0600

    day four

---
+++ day-04/practice.js
@@ -0,0 +1,11 @@
+console.log("this is comming from practice js");
+
+let catchingBus = true;
+
+if (catchingBus) {
+    console.log("I will reach home on time.");
+    
+} else {
+    console.log("I will be late to reach.");
+    
+} 
