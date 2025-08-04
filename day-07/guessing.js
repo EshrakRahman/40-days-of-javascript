7c2d74073f6ff048e8da34489fa83f1d154f6144
Eshrak Rahman <eshrakurrahman@gmail.com>
2025-08-04 23:27:33 +0600

    day-07 task completed

--- day-07/guessing.js
+++ day-07/guessing.js
@@ -1,1 +1,41 @@
 console.log("connected");
+
+
+
+function gussingANumberGame () {
+    let userPrompt = Number(prompt("Guess a number between 1 to 10"));
+
+    const minNum = 1;
+    const maxNum = 10;
+    console.log(userPrompt);
+    
+    let randomNumber = Math.floor(Math.random() * maxNum) + 1;
+    console.log(randomNumber);
+
+    let count = 1;
+    for( let i = minNum; i <= maxNum; i++) {
+
+        if (userPrompt > randomNumber) {
+            console.log("Your guess is high");
+            let newPromtp = Number(prompt("Guess a number between 1 to 10 again"));
+            count++;
+            if (randomNumber === newPromtp) {
+                console.log(`your won with ${count} times`);
+                break;
+            }
+            
+        } 
+        if (userPrompt < randomNumber) {
+            console.log("Your guess is low");
+            let newPromtp = Number(prompt("Guess a number between 1 to 10 again"));
+            count++;
+            if (randomNumber === newPromtp) {
+                console.log(`your won with ${count} times`);
+                break;
+            }
+        }
+        
+    }
+}
+
+gussingANumberGame();
